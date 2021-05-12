9fe0f62a438df349cc5e93327e1c83c914cbfd78
Alexandre BOUDET <alexandreboudet35@gmail.com>
2021-05-12 19:33:17 +0200

    Ajout du repérage des amers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Map } from './components/Map';
 import { TopBar } from './components/TopBar';
+import { Canvas } from './components/Canvas';
 
 export const App = () => {
     return (
         <>
             <TopBar />
             <Map />
+            <Canvas />
         </>
     );
 };
